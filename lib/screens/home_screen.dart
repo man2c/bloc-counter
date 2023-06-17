c346944f01c51fabeafd6ecf699f865f3da7f34e
chamara <chamara@misynergy.com>
2023-06-18 01:31:26 +0530

    feature/C-1 Splash screen implement.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,9 +3,25 @@ import 'package:counter/events/counter_event.dart';
 import 'package:counter/states/counter_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
